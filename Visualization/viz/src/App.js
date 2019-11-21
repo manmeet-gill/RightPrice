b7da989103e512b22d91d17b4b730bd8508ae075
Karthik-Tella <karthiktel11@gmail.com>
2019-11-20 19:07:30 -0800

    visulization components react

---
+++ Visualization/viz/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Bc from "./bc";
+import Sc from "./scatter";
+
+function App() {
+  return (
+    <div>
+      <Bc />
+      <Sc />
+    </div>
+  );
+}
+
+export default App;
